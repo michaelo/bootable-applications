aa1d2ebb9c273c5d1f2b44fad6644021d6cfd9f1
Michael Odden <me@michaelodden.com>
2025-05-04 00:19:15 +0200

    examples/text: vendors font8x8-repo + implements support for ext_latin chars

--- examples/text/text.c
+++ examples/text/text.c
@@ -4,17 +4,36 @@
 #include "shared/draw.c"
 #include "shared/screen_selectres.c"
 
-#include "font8x8_basic.h"
+#include "font8x8/font8x8_latin.h"
+
+char* getbitmap(int ord)
+{
+    if(ord < 0) return NULL;
+
+    if(ord <= 0x7F) {
+        // Contains an 8x8 font map for unicode points U+0000 - U+007F (basic latin)
+        return font8x8_basic[ord];
+    } else if (ord <= 0x9F) {
+        // Contains an 8x8 font map for unicode points U+0080 - U+009F (C1/C2 control)
+        return font8x8_control[ord - 0x80];
+    } else if (ord <= 0xFF) {
+        // Contains an 8x8 font map for unicode points U+00A0 - U+00FF (extended latin)
+        return font8x8_ext_latin[ord - 0xA0];
+    }
+    return NULL;
+}
 
 void renderChar(EFI_GRAPHICS_OUTPUT_PROTOCOL *gfx, int dx, int dy, EFI_GRAPHICS_OUTPUT_BLT_PIXEL bg, EFI_GRAPHICS_OUTPUT_BLT_PIXEL fg, int ord)
 {
-    if (ord < 0 || ord > 127)
+    if (ord < 0 || ord > 255)
         return;
 
     EFI_GRAPHICS_OUTPUT_BLT_PIXEL *frame_buffer_addr = (EFI_GRAPHICS_OUTPUT_BLT_PIXEL *)gfx->Mode->frame_buffer_base;
     EFI_UINT64 frame_buffer_size = gfx->Mode->frame_buffer_size;
 
-    char *bitmap = font8x8_basic[ord];
+    char *bitmap = getbitmap(ord);
+    if(!bitmap) return;
+    
     int x, y;
     int set;
     int mask;
@@ -30,44 +49,78 @@ void renderChar(EFI_GRAPHICS_OUTPUT_PROTOCOL *gfx, int dx, int dy, EFI_GRAPHICS_
     }
 }
 
-void renderString(EFI_GRAPHICS_OUTPUT_PROTOCOL *gfx, int dx, int dy, EFI_GRAPHICS_OUTPUT_BLT_PIXEL bg, EFI_GRAPHICS_OUTPUT_BLT_PIXEL fg, char *text)
+// Transparent bg
+void renderCharFg(EFI_GRAPHICS_OUTPUT_PROTOCOL *gfx, int dx, int dy, EFI_GRAPHICS_OUTPUT_BLT_PIXEL fg, int ord)
+{
+    if (ord < 0 || ord > 255)
+        return;
+
+    EFI_GRAPHICS_OUTPUT_BLT_PIXEL *frame_buffer_addr = (EFI_GRAPHICS_OUTPUT_BLT_PIXEL *)gfx->Mode->frame_buffer_base;
+    EFI_UINT64 frame_buffer_size = gfx->Mode->frame_buffer_size;
+
+    char *bitmap = getbitmap(ord);
+    if(!bitmap) return;
+
+    int x, y;
+    int set;
+    int mask;
+    for (x = 0; x < 8; x++)
+    {
+        for (y = 0; y < 8; y++)
+        {
+            set = bitmap[y] & 1 << x;
+            EFI_UINT64 idx = (dy + y) * gfx->Mode->info->HorizontalResolution + (dx + x);
+            if(idx >= frame_buffer_size) continue;
+            if(set) frame_buffer_addr[idx] = fg;
+        }
+    }
+}
+
+// void renderString(EFI_GRAPHICS_OUTPUT_PROTOCOL *gfx, int dx, int dy, EFI_GRAPHICS_OUTPUT_BLT_PIXEL bg, EFI_GRAPHICS_OUTPUT_BLT_PIXEL fg, char *text)
+// {
+//     // assumes text is eventually null-terminated
+//     int cidx = 0;
+//     while(text[cidx] != 0) {
+//         renderChar(gfx, dx+(8*cidx), dy, bg, fg, text[cidx]);
+//         cidx += 1;
+//     }
+// }
+
+void renderStringFg(EFI_GRAPHICS_OUTPUT_PROTOCOL *gfx, int dx, int dy, EFI_GRAPHICS_OUTPUT_BLT_PIXEL fg, EFI_UINT16 *text)
 {
     // assumes text is eventually null-terminated
     int cidx = 0;
     while(text[cidx] != 0) {
-        renderChar(gfx, dx+(8*cidx), dy, bg, fg, text[cidx]);
+        renderCharFg(gfx, dx+(8*cidx), dy, fg, text[cidx]);
         cidx += 1;
     }
 }
 
 EFI_UINTN EfiMain(EFI_HANDLE handle, EFI_SYSTEM_TABLE *system_table)
 {
-    EFI_UINTN event;
-    EFI_INPUT_KEY key;
-
     // Prevent automatic 5min abort
     system_table->BootServices->SetWatchdogTimer(0, 0, 0, NULL);
 
     EFI_GRAPHICS_OUTPUT_PROTOCOL *gfx = GetModeGraphics(system_table);
-
-    // For brevity
     EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *out = system_table->ConOut;
-
     out->ClearScreen(out);
 
-    // out->SetCursorPosition(out, 0, 0);
-    // out->OutputString(out, L"Hello");
-
     SelectResolution(system_table);
-    drawRectangle(gfx, 10, 10, 10, 10, color(128, 0, 0));
+    drawRectangle(gfx, 10, 10, 95, 95, color(128, 0, 0));
+
+    drawRectangle(gfx, 150, 70, 95, 120, color(128, 128, 0));
 
     EFI_GRAPHICS_OUTPUT_BLT_PIXEL bg = color(0, 0, 0);
     EFI_GRAPHICS_OUTPUT_BLT_PIXEL fg = color(255, 255, 255);
 
     // renderChar(gfx, 100, 100, bg, fg, 'A');
-    renderString(gfx, 100, 100, bg, fg, "Woop woop - this is all good!");
+    // renderString(gfx, 100, 100, bg, fg, L"Woop woop - this is all good! ha æøå da");
+    renderStringFg(gfx, 100, 100, fg, L"Woop woop - this is all good! ha æøå da");
+
+    renderStringFg(gfx, 100, 200, fg, L"Press Enter to shutdown");
 
     BlockForKey(system_table, 13);
+    system_table->RuntimeServices->ResetSystem(EFI_RESET_TYPE_Shutdown, 0, 0, NULL);
 
     return (0);
 }
