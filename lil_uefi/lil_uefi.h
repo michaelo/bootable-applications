f2ac20604a77bd250f1e5686a59509cf87f3229f
Michael Odden <me@michaelodden.com>
2025-05-12 00:30:48 +0200

    Adds Loaded Image Protocol (Section 1.1) to lil_uefi

--- lil_uefi/lil_uefi.h
+++ lil_uefi/lil_uefi.h
@@ -537,6 +537,44 @@ typedef struct EFI_RUNTIME_SERVICES{
 } EFI_RUNTIME_SERVICES;
 
 ////////////////////////////////
+// Protocol: Loaded Image Protocol
+#define EFI_LOADED_IMAGE_PROTOCOL_GUID \
+    EFI_GuidVal(0x5B1B31A1,0x9562,0x11d2, 0x8E,0x3F,0x00,0xA0,0xC9,0x69,0x72,0x3B)
+
+#define EFI_LOADED_IMAGE_PROTOCOL_REVISION 0x1000
+
+// Simple Text Input Protocol function table
+#define X(N, R, P) typedef R EFIAPI EFI_LoadedImageProtocol_##N P;
+#include "lil_uefi_loaded_image_funcs.h"
+#undef X
+
+typedef struct EFI_LOADED_IMAGE_PROTOCOL{
+    EFI_UINT32                    Revision;
+    EFI_HANDLE                    ParentHandle;
+    EFI_SYSTEM_TABLE              *SystemTable;
+    
+    // Source location of the image
+    EFI_HANDLE                    DeviceHandle;
+    struct EFI_DEVICE_PATH_PROTOCOL      *FilePath;
+    void                          *Reserved;
+    
+    // Image’s load options
+    EFI_UINT32                    LoadOptionsSize;
+    void                          *LoadOptions;
+    
+    // Location where image was loaded
+    void                          *ImageBase;
+    EFI_UINT64                    ImageSize;
+    EFI_MEMORY_TYPE               ImageCodeType;
+    EFI_MEMORY_TYPE               ImageDataType;
+#define X(N, R, P) EFI_LoadedImageProtocol_##N *N;
+#include "lil_uefi_loaded_image_funcs.h"
+#undef X
+    // EFI_EVENT WaitForKey;
+} EFI_LOADED_IMAGE_PROTOCOL;
+
+
+////////////////////////////////
 // Protocol: Device Path Protocol
 
 // SPEC: Section 10.2
---
+++ lil_uefi/lil_uefi_loaded_image_funcs.h
@@ -0,0 +1,4 @@
+// SPEC: Section 9.1
+
+// EFI_IMAGE_UNLOAD
+X(Unload, EFI_STATUS, (EFI_HANDLE ImageHandle))
