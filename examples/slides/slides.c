d7df25d67d3614cf2d907a2e2de39d5a998baf27
Michael Odden <me@michaelodden.com>
2025-05-23 02:41:38 +0200

    Adds examples/slides to start exploring implementing slide deck as BA

---
+++ examples/slides/slides.c
@@ -0,0 +1,130 @@
+
+#include "lil_uefi/lil_uefi.h"
+#include "shared/rand.h"
+#include "shared/draw.h"
+#include "shared/math.h"
+#include "shared/hsv.h"
+#include "shared/memory.h"
+#include "shared/text.h"
+
+static EFI_INT8 qr_plot[33][33] = {
+    {1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1},
+    {1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1},
+    {1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1},
+    {1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0},
+    {0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1},
+    {0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0},
+    {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0},
+    {0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0},
+    {1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1},
+    {0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0},
+    {1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0},
+    {1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1},
+    {0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1},
+    {1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0},
+    {1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0},
+    {1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0},
+    {1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1},
+    {1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0},
+    {1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0},
+    {1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0},
+    {1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1},
+    {1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1},
+    {1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1},
+    {1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0},
+    {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0}};
+
+void plotToBitmap(Bitmap *bitmap, EFI_INT8 *plot, EFI_UINTN dx, EFI_UINTN dy, EFI_UINTN plot_width, EFI_UINTN plot_height, Color_BGRA color_fg, Color_BGRA color_bg)
+{
+    // TBD: Support "CLUT" to map plot from index to color?
+    EFI_UINTN width = MIN(plot_width, bitmap->width);
+    EFI_UINTN height = MIN(plot_height, bitmap->height);
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            bitmap->buffer[bitmap->stride * (y + dy) + (x + dx)] = plot[y * plot_width + x] > 0 ? color_fg : color_bg;
+        }
+    }
+}
+
+EFI_STATUS EfiMain(EFI_HANDLE handle, EFI_SYSTEM_TABLE *system_table)
+{
+    EFI_GRAPHICS_OUTPUT_PROTOCOL *gfx = GetModeGraphics(system_table);
+    Memory mem = initializeMemory(system_table->BootServices);
+    EFI_CHAR16 scratch[128];
+
+    // Reference resolution: 1440 x 900
+    // 16:10
+    // Bitmap *bmp_canvas = allocateBitmap(1440, 900, &mem);
+    // Reference resolution: 1280 x 800
+    Bitmap *bmp_canvas = allocateBitmap(1280, 800, &mem);
+
+    // Create bitmap for QR code
+    Bitmap *bmp_qr = allocateBitmap(35, 35, &mem);
+    fillBitmap(bmp_qr, color(0, 0, 0));
+    plotToBitmap(bmp_qr, (EFI_INT8 *)qr_plot, 1, 1, 33, 33, color(255, 255, 255), color(0, 0, 0));
+
+    // Wrap screen buffer info in a bitmap-struct to utilize bitmap.h-functionality
+    Bitmap screen;
+    initializeBitmapFromScreenBuffer(&screen, gfx);
+
+    // Create bitmap for title
+    Bitmap *bmp_title = allocateBitmap(900, 80, &mem);
+    fillBitmap(bmp_title, color(0, 0, 0));
+    Color_BGRA color_outline = color(255, 255, 255);
+    Color_BGRA color_text = color(0, 0, 0);
+    renderStringOutline(bmp_title, 3, 3, color_text, color_outline, 3, 42, L"Bootable Applications");
+    renderStringOutline(bmp_title, 3, 46, color_text, color_outline, 3, 32, L"- an introduction");
+
+    EFI_UINTN padding = 8;
+
+    int title_x = padding;
+    int title_y = (bmp_canvas->height-bmp_title->height)/2;
+
+    // Blit text
+    fillBitmap(bmp_canvas, color(0, 0, 0));
+    // fillBitmap(bmp_title, color(255, 0, 0));
+    bltBitmapScaled(bmp_canvas, bmp_title, title_x, title_y, title_x + bmp_title->width, title_y + bmp_title->height);
+
+    // Blit QR
+    float qr_size=140*2;
+    int qr_y = (bmp_canvas->height-qr_size)/2;
+    bltBitmapScaled(bmp_canvas, bmp_qr,
+                    bmp_canvas->width - qr_size, // x start
+                    qr_y,
+                    bmp_canvas->width, // x end
+                    qr_y + qr_size);
+
+    // Render to screen
+    bltBitmapScaled(&screen, bmp_canvas, 0, 0, screen.width, screen.height);
+
+    // Block until <Enter>
+    BlockForKey(system_table, 13);
+
+cleanup:
+    if (bmp_canvas != NULL)
+    {
+        mem.free(&mem, bmp_canvas);
+    }
+
+    if (bmp_qr != NULL)
+    {
+        mem.free(&mem, bmp_qr);
+    }
+
+    if (bmp_title != NULL)
+    {
+        mem.free(&mem, bmp_title);
+    }
+
+    return 0;
+}
