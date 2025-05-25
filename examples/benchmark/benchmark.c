f61b1d1a6f2d57339973120d4619a26d3fd21db7
Terje Wiesener <wiesener@gmail.com>
2025-05-25 22:32:59 +0200

    Added some more benchmarks

--- examples/benchmark/benchmark.c
+++ examples/benchmark/benchmark.c
@@ -7,34 +7,100 @@
 #include "shared/text.h"
 
 #define SCRATCH_SIZE 1024
+#define NAME_SIZE 32
 
-void benchmark(void (*func)(void), int num, EFI_EVENT * event, int * count, int * time)
+typedef struct Benchmark_s {
+    short name[NAME_SIZE];
+    float timeSeconds;
+    int num;
+    void (*target)(void *);
+    void * state;
+} Benchmark;
+
+Benchmark createBenchmark(unsigned short name[], void (*target)(void *), void * state)
 {
-    *count = 0;
-    *time = 0;
-    while (++(*count) < num){
-        func();
-        if (!efi_global_system_table->BootServices->CheckEvent(event)) (*time)++;
+    Benchmark benchmark = {
+        .timeSeconds = 3,
+        .num = 0,
+        .target = target,
+        .state = state
+    };
+    memcpy(benchmark.name, name, NAME_SIZE);
+    return benchmark;
+}
+
+#define EFI_ERROR(status) (status != 0)
+
+void runBenchmark(Benchmark * benchmark)
+{
+    int second = 10000000;
+    benchmark->num = 0;
+
+    EFI_STATUS Status;
+    EFI_EVENT TimerEvent;
+    Status = efi_boot_services()->CreateEvent(EFI_EVT_TIMER, EFI_TPL_CALLBACK, NULL, (void *)NULL, &TimerEvent);
+    if (EFI_ERROR (Status)) {
+        benchmark->num = Status;
+        return;
     }
+
+    Status = efi_boot_services()->SetTimer(TimerEvent, EFI_TIMER_DELAY_Relative, (int)(second * benchmark->timeSeconds));
+    if (EFI_ERROR (Status)) {
+        efi_boot_services()->CloseEvent (TimerEvent);
+        benchmark->num = Status;
+        return;
+    }
+
+    do {
+        Status = efi_boot_services()->CheckEvent (TimerEvent);
+        benchmark->target(benchmark->state);
+        benchmark->num++;
+    } while (EFI_ERROR (Status));
+
+    efi_boot_services()->CloseEvent(TimerEvent);
 }
 
 EFI_GRAPHICS_OUTPUT_PROTOCOL *gfx_out;
 Bitmap screen;
+Color_BGRA orange;
 Color_BGRA pink;
+Bitmap * backBuffer;
 
-void fillScreenNaive()
+int white = 0xff;
+
+void fillScreenNaive(void * state)
 {
-    fillBitmap(&screen, pink);
+    fillBitmap(&screen, *(Color_BGRA*)state);
 }
 
-void fillScreenBlt()
+void fillScreenBlt(void * state)
 {
-    fillScreen(gfx_out, pink);
+    fillScreen(gfx_out, *(Color_BGRA*)state);
 }
 
-void fillScreenMemset()
+void fillScreenMemset(void * state)
 {
-    memset(screen.buffer, 0xff, screen.stride * screen.height * 4);
+    memset(screen.buffer, *(int*)state, screen.stride * screen.height * 4);
+}
+
+void bitmapScreenBlt(void * state)
+{
+    drawBitmapToScreen(gfx_out, 0, 0, backBuffer);
+}
+
+void bitmapScreenMemcpy(void * state)
+{
+    memcpy(screen.buffer, backBuffer->buffer, backBuffer->stride * backBuffer->height * 4);
+}
+
+void fillBitmapNaive(void * state)
+{
+    fillBitmap(backBuffer, *(Color_BGRA*)state);
+}
+
+void fillBitmapMemset(void * state)
+{
+    memset(backBuffer->buffer, *(int*)state, screen.stride * screen.height * 4);
 }
 
 // entry point
@@ -55,39 +121,40 @@ EFI_UINTN EfiMain(EFI_HANDLE handle, EFI_SYSTEM_TABLE *system_table)
     gfx_out->SetMode(gfx_out, 0);
 
     initializeBitmapFromScreenBuffer(&screen, gfx_out);    
-    pink = color(255,128,0);
+    backBuffer = allocateBitmap(screen.width, screen.height);
+
+    orange = color(255,128,0);
+    pink = color(255,200,200);
     unsigned short text[SCRATCH_SIZE];
-
-
-    int second = 10000000;
-    int ms = second / 1000;
-    EFI_UINTN index;
-    EFI_EVENT timerEvent;
-    boot_services->CreateEvent(EFI_EVT_TIMER, EFI_TPL_CALLBACK, NULL, (void *)NULL, &timerEvent);
-    boot_services->SetTimer(timerEvent, EFI_TIMER_DELAY_Periodic, ms * 10);
 
     EFI_UINTN keyEvent;
     EFI_INPUT_KEY key;
+
+    Benchmark benchmarks[] = {
+        createBenchmark(L"FillScreenNaive", fillScreenNaive, &pink),
+        createBenchmark(L"FillScreenMemSet", fillScreenMemset, &white),
+        createBenchmark(L"FillScreenBlt", fillScreenBlt, &orange),
+        createBenchmark(L"FillBitmapNaive", fillBitmapNaive, &pink),
+        createBenchmark(L"FillBitmapMemSet", fillBitmapMemset, &white),
+        createBenchmark(L"BitmapScreenBlt", bitmapScreenBlt, 0),
+        createBenchmark(L"BitmapScreenMemcpy", bitmapScreenMemcpy, &white),
+    };
+
     for (;;)
     {
         system_table->BootServices->WaitForEvent(1, &system_table->ConIn->WaitForKey, &keyEvent);
         system_table->ConIn->ReadKeyStroke(system_table->ConIn, &key);
 
-        int bltCount, naiveCount, memsetCount;
-        int bltTime, naiveTime, memsetTime;
+        for(int i = 0; i < 7; i++)
+        {
+            runBenchmark(&benchmarks[i]);            
+        }
 
-        benchmark(fillScreenBlt, 5000, timerEvent, &bltCount, &bltTime);
-        benchmark(fillScreenMemset, 5000, timerEvent, &memsetCount, &memsetTime);
-        benchmark(fillScreenNaive, 5000, timerEvent, &naiveCount, &naiveTime);
-
-        FormatterZ(text, SCRATCH_SIZE, L"Blt: \t %d iterations in approx %d ms", bltCount, bltTime * 10);
-        renderStringOutline(&screen, 50, 100, color(0,0,0), color(255,255,255), 1, 8, text);
-        
-        FormatterZ(text, SCRATCH_SIZE, L"Memset: \t %d iterations in approx %d ms", memsetCount, memsetTime * 10);
-        renderStringOutline(&screen, 50, 200, color(0,0,0), color(255,255,255), 1, 8, text);
-
-        FormatterZ(text, SCRATCH_SIZE, L"Naive: \t %d iterations in approx %d ms", naiveCount, naiveTime * 10);
-        renderStringOutline(&screen, 50, 150, color(0,0,0), color(255,255,255), 1, 8, text);
+        for(int i = 0; i < 7; i++)
+        {
+            FormatterZ(text, SCRATCH_SIZE, L"%s: %d iterations in approx %d ms", benchmarks[i].name, benchmarks[i].num, (int)(benchmarks[i].timeSeconds * 1000));
+            renderStringOutline(&screen, 50, 50 + (i * 16), color(0,0,0), color(255,255,255), 1, 8, text);
+        }
     }
 
     return 0;
