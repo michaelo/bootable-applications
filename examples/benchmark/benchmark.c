00fa70b470da67f3f7d0e1aa67aeb2e11ba94c18
Terje Wiesener <wiesener@gmail.com>
2025-05-25 23:10:58 +0200

    Fixed broken benchmark

--- examples/benchmark/benchmark.c
+++ examples/benchmark/benchmark.c
@@ -7,7 +7,7 @@
 #include "shared/text.h"
 
 #define SCRATCH_SIZE 1024
-#define NAME_SIZE 32
+#define NAME_SIZE 60
 
 typedef struct Benchmark_s {
     short name[NAME_SIZE];
@@ -20,7 +20,7 @@ typedef struct Benchmark_s {
 Benchmark createBenchmark(unsigned short name[], void (*target)(void *), void * state)
 {
     Benchmark benchmark = {
-        .timeSeconds = 3,
+        .timeSeconds = 1,
         .num = 0,
         .target = target,
         .state = state
@@ -64,6 +64,8 @@ EFI_GRAPHICS_OUTPUT_PROTOCOL *gfx_out;
 Bitmap screen;
 Color_BGRA orange;
 Color_BGRA pink;
+Color_BGRA green;
+
 Bitmap * backBuffer;
 
 int white = 0xff;
@@ -121,10 +123,12 @@ EFI_UINTN EfiMain(EFI_HANDLE handle, EFI_SYSTEM_TABLE *system_table)
     gfx_out->SetMode(gfx_out, 0);
 
     initializeBitmapFromScreenBuffer(&screen, gfx_out);    
-    backBuffer = allocateBitmap(screen.width, screen.height);
+    backBuffer = allocateBitmap(screen.stride, screen.height);
+    backBuffer->width = screen.width;
 
     orange = color(255,128,0);
     pink = color(255,200,200);
+    green = color(0,255,0);
     unsigned short text[SCRATCH_SIZE];
 
     EFI_UINTN keyEvent;
@@ -134,23 +138,25 @@ EFI_UINTN EfiMain(EFI_HANDLE handle, EFI_SYSTEM_TABLE *system_table)
         createBenchmark(L"FillScreenNaive", fillScreenNaive, &pink),
         createBenchmark(L"FillScreenMemSet", fillScreenMemset, &white),
         createBenchmark(L"FillScreenBlt", fillScreenBlt, &orange),
-        createBenchmark(L"FillBitmapNaive", fillBitmapNaive, &pink),
         createBenchmark(L"FillBitmapMemSet", fillBitmapMemset, &white),
         createBenchmark(L"BitmapScreenBlt", bitmapScreenBlt, 0),
-        createBenchmark(L"BitmapScreenMemcpy", bitmapScreenMemcpy, &white),
+        createBenchmark(L"FillBitmapNaive", fillBitmapNaive, &green),
+        createBenchmark(L"BitmapScreenMemcpy", bitmapScreenMemcpy, 0),
     };
+
+    int numBenchmarks = sizeof(benchmarks) / sizeof(Benchmark);
 
     for (;;)
     {
         system_table->BootServices->WaitForEvent(1, &system_table->ConIn->WaitForKey, &keyEvent);
         system_table->ConIn->ReadKeyStroke(system_table->ConIn, &key);
 
-        for(int i = 0; i < 7; i++)
+        for(int i = 0; i < numBenchmarks; i++)
         {
             runBenchmark(&benchmarks[i]);            
         }
 
-        for(int i = 0; i < 7; i++)
+        for(int i = 0; i < numBenchmarks; i++)
         {
             FormatterZ(text, SCRATCH_SIZE, L"%s: %d iterations in approx %d ms", benchmarks[i].name, benchmarks[i].num, (int)(benchmarks[i].timeSeconds * 1000));
             renderStringOutline(&screen, 50, 50 + (i * 16), color(0,0,0), color(255,255,255), 1, 8, text);
